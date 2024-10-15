c0ffbce16d0f1468259cfd54e7734f82fbefa01b
Nasser Anssari <nanssari@expediagroup.com>
2024-10-15 17:14:12 +0300

    chore: update examples

--- examples/src/main/java/com/expediagroup/sdk/xap/examples/scenarios/lodging/GetListingsScenario.java
+++ examples/src/main/java/com/expediagroup/sdk/xap/examples/scenarios/lodging/GetListingsScenario.java
@@ -14,18 +14,13 @@
  * limitations under the License.
  */
 
-package com.expediagroup.sdk.xap.examples.scenarios.lodging.shopping.listings;
+package com.expediagroup.sdk.xap.examples.scenarios.lodging;
 
-import com.expediagroup.sdk.xap.examples.Constants;
+import com.expediagroup.sdk.xap.examples.scenarios.XapScenario;
+import com.expediagroup.sdk.xap.examples.services.LodgingService;
 import com.expediagroup.sdk.xap.models.Hotel;
 import com.expediagroup.sdk.xap.models.HotelListingsResponse;
-import com.expediagroup.sdk.xap.models.Room;
 import com.expediagroup.sdk.xap.models.RoomType;
-import com.expediagroup.sdk.xap.operations.GetLodgingListingsOperation;
-import com.expediagroup.sdk.xap.operations.GetLodgingListingsOperationParams;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,16 +29,14 @@ import org.slf4j.LoggerFactory;
  * This example demonstrates how to search for properties with a location keyword with filters
  * applied.
  */
-public class ListingsQuickStartExample {
+public class GetListingsScenario implements XapScenario {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ListingsQuickStartExample.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(GetListingsScenario.class);
 
-  /**
-   * Summary: main function.
-   */
-  public static void main(String[] args) {
-    LOGGER.info("========== Start QuickStartExample ==========");
+  private final LodgingService lodgingService = new LodgingService();
 
+  @Override
+  public void run() {
     // This example will search for properties with the following criteria:
     // 1. Occupancy of 1 adult in the first room and 2 adults and 2 children (10 and 12 years old)
     // in the second room;
@@ -53,58 +46,16 @@ public class ListingsQuickStartExample {
     // 5. Limit the results to 5 properties;
     // 6. Order the results by price in ascending order.
 
-    // Build the occupancy
-    ArrayList<Room> rooms = new ArrayList<>();
-    // The first room, with 1 adult
-    rooms.add(Room.builder().adults(1L).build());
-    // The second room, with 2 adults and 2 children
-    ArrayList<Long> childrenAges = new ArrayList<>();
-    childrenAges.add(10L);
-    childrenAges.add(12L);
-    rooms.add(Room.builder().adults(2L).childAges(childrenAges).build());
+    LOGGER.info(
+        "============================= Running GetListingsScenario ============================");
 
-    // Build the query parameters with GetLodgingListingsOperationParams
-    GetLodgingListingsOperationParams getLodgingListingsOperationParams =
-        GetLodgingListingsOperationParams.builder()
-            .partnerTransactionId(Constants.PARTNER_TRANSACTION_ID)
-            // The location keyword can be a city, address, airport or a landmark.
-            .locationKeyword("Space Needle, Seattle")
-            // The radius specifies the size of search area around the location keyword.
-            // The default value is 25.
-            .radius(10)
-            // The unit specifies the unit of the radius. The default value is KM.
-            .unit(GetLodgingListingsOperationParams.Unit.KM)
-            // Check-in 5 days from now
-            .checkIn(LocalDate.now().plusDays(5))
-            // Check-out 10 days from now
-            .checkOut(LocalDate.now().plusDays(10))
-            // The occupancy
-            .rooms(rooms)
-            // The links to return, WEB includes WS (Web Search Result Page)
-            // and WD (Web Details Page)
-            .links(Collections.singletonList(GetLodgingListingsOperationParams.Links.WEB))
-            // Limit the results to 5 properties
-            .limit(5)
-            // Order the results by price in ascending order
-            .sortType(GetLodgingListingsOperationParams.SortType.PRICE)
-            .sortOrder(GetLodgingListingsOperationParams.SortOrder.ASC)
-            .build();
+    LOGGER.info(
+        "======================== Executing GetLodgingListingsOperation =======================");
 
-    // Execute the operation and get the HotelListingsResponse
-    LOGGER.info("========== Executing GetLodgingListingsOperation ==========");
-    HotelListingsResponse hotelListingsResponse = Constants.XAP_CLIENT.execute(
-        new GetLodgingListingsOperation(getLodgingListingsOperationParams)).getData();
-    // If you want to use the async method, you can use the following code:
-    // ---------------------------------------------------------------
-    // CompletableFuture<Response<HotelListingsResponse>> completableFuture =
-    //   Constants.XAP_CLIENT.executeAsync(
-    //     new GetLodgingListingsOperation(getLodgingListingsOperationParams));
-    // completableFuture.thenAccept(hotelListingsResponse -> {
-    //   // Your code here
-    // });
-    // ---------------------------------------------------------------
+    HotelListingsResponse hotelListingsResponse = lodgingService.getListings();
 
-    LOGGER.info("========== GetLodgingListingsOperation Executed ==========");
+    LOGGER.info(
+        "======================== GetLodgingListingsOperation Executed ========================");
 
     if (hotelListingsResponse == null || hotelListingsResponse.getHotels() == null
         || hotelListingsResponse.getHotels().isEmpty()) {
@@ -116,7 +67,8 @@ public class ListingsQuickStartExample {
 
     // To access the properties, iterate through the list of hotel properties
     hotelListingsResponse.getHotels().forEach(hotel -> {
-      LOGGER.info("========== Property Start ==========");
+      LOGGER.info(
+          "=================================== Property Start ===================================");
       // Check if the property is available
       if (Hotel.Status.AVAILABLE != hotel.getStatus()) {
         LOGGER.info("Property is not available.");
@@ -186,10 +138,11 @@ public class ListingsQuickStartExample {
           }
         }
       }
-      LOGGER.info("========== Property End ==========");
+      LOGGER.info(
+          "==================================== Property End ====================================");
     });
 
-    LOGGER.info("========== End QuickStartExample ==========");
-    System.exit(0);
+    LOGGER.info(
+        "=============================== End GetListingsScenario ==============================");
   }
 }
