7817490b7314ae3cbb1da28e14a05c02ba894a76
Nasser Anssari <nanssari@expediagroup.com>
2024-11-07 14:11:40 +0300

    feat: update transacation-id header and comma-separated string params (#16)

--- customizations/generator/openapi/src/main/resources/post-processor/assets/templates/get-lodging-listings-operation-params/rooms.kt
+++ customizations/generator/openapi/src/main/resources/post-processor/assets/templates/get-lodging-listings-operation-params/rooms.kt
@@ -2,41 +2,41 @@ fun rooms(rooms: List<Room>) =
     apply {
         rooms.elementAtOrNull(0)?.let {
             this.room1Adults = it.adults
-            this.room1ChildAges = it.childAges?.joinToString(",")
+            this.room1ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(1)?.let {
             this.room2Adults = it.adults
-            this.room2ChildAges = it.childAges?.joinToString(",")
+            this.room2ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(2)?.let {
             this.room3Adults = it.adults
-            this.room3ChildAges = it.childAges?.joinToString(",")
+            this.room3ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(3)?.let {
             this.room4Adults = it.adults
-            this.room4ChildAges = it.childAges?.joinToString(",")
+            this.room4ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(4)?.let {
             this.room5Adults = it.adults
-            this.room5ChildAges = it.childAges?.joinToString(",")
+            this.room5ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(5)?.let {
             this.room6Adults = it.adults
-            this.room6ChildAges = it.childAges?.joinToString(",")
+            this.room6ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(6)?.let {
             this.room7Adults = it.adults
-            this.room7ChildAges = it.childAges?.joinToString(",")
+            this.room7ChildAges = it.childAges?.map { item -> item.toString() }
         }
 
         rooms.elementAtOrNull(7)?.let {
             this.room8Adults = it.adults
-            this.room8ChildAges = it.childAges?.joinToString(",")
+            this.room8ChildAges = it.childAges?.map { item -> item.toString() }
         }
     }
