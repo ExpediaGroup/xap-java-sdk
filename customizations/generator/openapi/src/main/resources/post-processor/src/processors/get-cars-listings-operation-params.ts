bc743b348eb6ae4ab81ad5ecfb57cb97a133406b
Nasser Anssari <nanssari@expediagroup.com>
2024-11-10 14:37:06 +0300

    post-process GetCarsListingsOperationParams

---
+++ customizations/generator/openapi/src/main/resources/post-processor/src/processors/get-cars-listings-operation-params.ts
@@ -0,0 +1,32 @@
+import {Edit, NapiConfig, SgNode} from '@ast-grep/napi';
+import {Processor} from './processor';
+import {RuleFunction} from './shared.types';
+
+export class NonCancellableDateTimeRangeProcessor extends Processor {
+  rules: RuleFunction[];
+  id: String = 'get-cars-listings-operation-params';
+
+  constructor() {
+    super();
+    this.rules = [
+      this.changeClassParamType,
+      this.changeBuilderMethodParamType,
+    ].map(rule => rule.bind(this));
+  }
+
+  changeClassParamType(root: SgNode): Edit[] {
+    const config = this.readRule('change-class-params-type');
+
+    return root.findAll(config).map(node => {
+      return node.replace('java.time.LocalDateTime');
+    });
+  }
+
+  changeBuilderMethodParamType(root: SgNode): Edit[] {
+    const config = this.readRule('change-builder-method-params-type');
+
+    return root.findAll(config).map(node => {
+      return node.replace('java.time.LocalDateTime');
+    });
+  }
+}
