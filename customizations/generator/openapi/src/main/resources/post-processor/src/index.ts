e445197ef0d35fa4e8ab5022c098a128e228ea4c
Nasser Anssari <nanssari@expediagroup.com>
2025-01-07 15:24:31 +0300

    chore: add activities post processor

--- customizations/generator/openapi/src/main/resources/post-processor/src/index.ts
+++ customizations/generator/openapi/src/main/resources/post-processor/src/index.ts
@@ -5,6 +5,7 @@ import {NonCancellableDateTimeRangeProcessor} from './processors/non-cancellable
 import {PenaltyRuleProcessor} from './processors/penalty-rule-processor';
 import {VendorLocationDetailsProcessor} from './processors/vendor-location-details-processor';
 import {GetCarsListingsOperationParamsProcessor} from './processors/get-cars-listings-operation-params-processor';
+import {ActivitiesCancellationPolicyProcessor} from "./processors/activities-cancellation-policy-processor";
 
 import * as path from 'path';
 
@@ -33,5 +34,8 @@ switch (fileName) {
     break;
   case 'GetCarsListingsOperationParams':
     new GetCarsListingsOperationParamsProcessor().process(filePath);
+    break;
+  case 'ActivitiesCancellationPolicy':
+    new ActivitiesCancellationPolicyProcessor().process(filePath);
     break;
 }
---
+++ customizations/generator/openapi/src/main/resources/post-processor/src/processors/activities-cancellation-policy-processor.ts
@@ -0,0 +1,32 @@
+import {Edit, NapiConfig, SgNode} from '@ast-grep/napi';
+import {Processor} from './processor';
+import {RuleFunction} from './shared.types';
+
+export class ActivitiesCancellationPolicyProcessor extends Processor {
+  rules: RuleFunction[];
+  id: String = 'car-cancellation-policy';
+
+  constructor() {
+    super();
+    this.rules = [
+      this.changeClassParamType,
+      this.changeBuilderMethodParamType,
+    ].map(rule => rule.bind(this));
+  }
+
+  changeClassParamType(root: SgNode): Edit[] {
+    const config = this.readRule('change-class-param-type');
+
+    return root.findAll(config).map(node => {
+      return node.replace('java.time.LocalDateTime');
+    });
+  }
+
+  changeBuilderMethodParamType(root: SgNode): Edit[] {
+    const config = this.readRule('change-builder-method-param-type');
+
+    return root.findAll(config).map(node => {
+      return node.replace('java.time.LocalDateTime');
+    });
+  }
+}
