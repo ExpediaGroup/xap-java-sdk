6692aa5fb89659dd6236a009de306cc82cafbdda
OmarAlJarrah <o.mazari.om63@gmail.com>
2025-03-17 06:00:42 +0300

    add signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import java.time.Duration
+
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 group = "com.expediagroup.sdk"
@@ -12,7 +17,19 @@ repositories {
     }
 }
 
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
 
+    transitionCheckOptions {
+        maxRetries.set(60)
+        delayBetween.set(Duration.ofMillis(5000))
+    }
+}
 
 dependencies {
     testImplementation(kotlin("test"))
--- xap-sdk/build.gradle.kts
+++ xap-sdk/build.gradle.kts
@@ -8,55 +8,11 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
-group = "com.expediagroup.sdk"
-version = "1.0.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
     maven {
         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
     }
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-    api("com.expediagroup:expediagroup-sdk-rest:0.0.1-beta-SNAPSHOT")
-    implementation("com.expediagroup:expediagroup-sdk-transport-okhttp:0.0.1-beta-SNAPSHOT")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
-    runtimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(21)
-    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
-}
-
-java {
-    withSourcesJar()
-    targetCompatibility = JavaVersion.VERSION_1_8
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-ktlint {
-    debug = true
-    version = "1.5.0"
-    verbose = true
-
-    additionalEditorconfig =
-        mapOf(
-            "max_line_length" to "200",
-            "indent_style" to "space",
-            "indent_size" to "4",
-            "insert_final_newline" to "true",
-            "end_of_line" to "lf",
-            "ktlint_standard_enum-entry-name-case" to "disabled",
-        )
 }
 
 publishing {
@@ -111,6 +67,65 @@ signing {
 
     useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications)
+}
+
+group = "com.expediagroup.sdk"
+version = "1.0.0-SNAPSHOT"
+
+
+//nexusPublishing {
+//    repositories {
+//        sonatype {
+//            username.set(System.getenv("SONATYPE_USERNAME"))
+//            password.set(System.getenv("SONATYPE_PASSWORD"))
+//        }
+//    }
+//
+//    transitionCheckOptions {
+//        maxRetries.set(60)
+//        delayBetween.set(Duration.ofMillis(5000))
+//    }
+//}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    api("com.expediagroup:expediagroup-sdk-rest:0.0.1-beta-SNAPSHOT")
+    implementation("com.expediagroup:expediagroup-sdk-transport-okhttp:0.0.1-beta-SNAPSHOT")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    runtimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+}
+
+java {
+    withSourcesJar()
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+ktlint {
+    debug = true
+    version = "1.5.0"
+    verbose = true
+
+    additionalEditorconfig =
+        mapOf(
+            "max_line_length" to "200",
+            "indent_style" to "space",
+            "indent_size" to "4",
+            "insert_final_newline" to "true",
+            "end_of_line" to "lf",
+            "ktlint_standard_enum-entry-name-case" to "disabled",
+        )
 }
 
 gradle.taskGraph.whenReady {
