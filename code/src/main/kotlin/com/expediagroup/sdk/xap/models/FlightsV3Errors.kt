780692a56c9bd3eee4289cca58415ebdaa28dbd0
Nasser Anssari <nanssari@expediagroup.com>
2025-04-11 15:04:12 +0300

    update code

---
+++ code/src/main/kotlin/com/expediagroup/sdk/xap/models/FlightsV3Errors.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2022 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.sdk.xap.models
+
+import com.expediagroup.sdk.core.model.exception.client.PropertyConstraintViolationException
+import com.expediagroup.sdk.xap.models.FlightsV3Error
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator
+import javax.validation.Valid
+import javax.validation.Validation
+import javax.validation.constraints.NotNull
+
+/**
+ *
+ * @param errors Container for error list.
+ * @param transactionId A unique identifier for the transaction.
+ */
+data class FlightsV3Errors(
+    // Container for error list.
+    @JsonProperty("Errors")
+    @field:NotNull
+    @field:Valid
+    val errors: kotlin.collections
+        .List<
+            FlightsV3Error
+        >,
+    // A unique identifier for the transaction.
+    @JsonProperty("TransactionId")
+    @field:NotNull
+    @field:Valid
+    val transactionId: kotlin.String
+) {
+    companion object {
+        @JvmStatic
+        fun builder() = Builder()
+    }
+
+    class Builder(
+        private var errors: kotlin.collections.List<FlightsV3Error>? = null,
+        private var transactionId: kotlin.String? = null
+    ) {
+        fun errors(errors: kotlin.collections.List<FlightsV3Error>) = apply { this.errors = errors }
+
+        fun transactionId(transactionId: kotlin.String) = apply { this.transactionId = transactionId }
+
+        fun build(): FlightsV3Errors {
+            val instance =
+                FlightsV3Errors(
+                    errors = errors!!,
+                    transactionId = transactionId!!
+                )
+
+            validate(instance)
+
+            return instance
+        }
+
+        private fun validate(instance: FlightsV3Errors) {
+            val validator =
+                Validation
+                    .byDefaultProvider()
+                    .configure()
+                    .messageInterpolator(ParameterMessageInterpolator())
+                    .buildValidatorFactory()
+                    .validator
+
+            val violations = validator.validate(instance)
+
+            if (violations.isNotEmpty()) {
+                throw PropertyConstraintViolationException(
+                    constraintViolations = violations.map { "${it.propertyPath}: ${it.message}" }
+                )
+            }
+        }
+    }
+
+    fun toBuilder() =
+        Builder(
+            errors = errors!!,
+            transactionId = transactionId!!
+        )
+}
---
+++ code/src/main/kotlin/com/expediagroup/sdk/xap/models/FlightsV3HotelLocation.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2022 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.sdk.xap.models
+
+import com.expediagroup.sdk.core.model.exception.client.PropertyConstraintViolationException
+import com.expediagroup.sdk.xap.models.FlightsV3Address
+import com.expediagroup.sdk.xap.models.FlightsV3GeoLocation
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator
+import javax.validation.Valid
+import javax.validation.Validation
+import javax.validation.constraints.NotNull
+
+/**
+ * Hotel address information
+ * @param address
+ * @param pointOfInterest
+ * @param geoLocation
+ */
+data class FlightsV3HotelLocation(
+    @JsonProperty("Address")
+    @field:NotNull
+    @field:Valid
+    val address: FlightsV3Address,
+    @JsonProperty("PointOfInterest")
+    @field:NotNull
+    @field:Valid
+    val pointOfInterest: kotlin.String,
+    @JsonProperty("GeoLocation")
+    @field:NotNull
+    @field:Valid
+    val geoLocation: FlightsV3GeoLocation
+) {
+    companion object {
+        @JvmStatic
+        fun builder() = Builder()
+    }
+
+    class Builder(
+        private var address: FlightsV3Address? = null,
+        private var pointOfInterest: kotlin.String? = null,
+        private var geoLocation: FlightsV3GeoLocation? = null
+    ) {
+        fun address(address: FlightsV3Address) = apply { this.address = address }
+
+        fun pointOfInterest(pointOfInterest: kotlin.String) = apply { this.pointOfInterest = pointOfInterest }
+
+        fun geoLocation(geoLocation: FlightsV3GeoLocation) = apply { this.geoLocation = geoLocation }
+
+        fun build(): FlightsV3HotelLocation {
+            val instance =
+                FlightsV3HotelLocation(
+                    address = address!!,
+                    pointOfInterest = pointOfInterest!!,
+                    geoLocation = geoLocation!!
+                )
+
+            validate(instance)
+
+            return instance
+        }
+
+        private fun validate(instance: FlightsV3HotelLocation) {
+            val validator =
+                Validation
+                    .byDefaultProvider()
+                    .configure()
+                    .messageInterpolator(ParameterMessageInterpolator())
+                    .buildValidatorFactory()
+                    .validator
+
+            val violations = validator.validate(instance)
+
+            if (violations.isNotEmpty()) {
+                throw PropertyConstraintViolationException(
+                    constraintViolations = violations.map { "${it.propertyPath}: ${it.message}" }
+                )
+            }
+        }
+    }
+
+    fun toBuilder() =
+        Builder(
+            address = address!!,
+            pointOfInterest = pointOfInterest!!,
+            geoLocation = geoLocation!!
+        )
+}
