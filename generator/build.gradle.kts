33c6156da990d5a7452455aeabbbdfed8e438c9a
OmarAlJarrah <o.mazari.om63@gmail.com>
2025-04-21 09:36:38 +0300

    chore: initial generator config

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -10,3 +10,41 @@ group = project.property("GROUP_ID") as String
 dependencies {
     api("org.openapitools:openapi-generator:7.11.0")
 }
+
+openApiGenerate {
+    inputSpec = System.getProperty("inputSpec") ?: "$projectDir/src/main/resources/specs.yaml"
+
+    packageName = "com.expediagroup.sdk.xap"
+    invokerPackage = "com.expediagroup.sdk.xap"
+    apiPackage = "com.expediagroup.sdk.xap.operations"
+    modelPackage = "com.expediagroup.sdk.xap.models"
+
+    dryRun = false
+    cleanupOutput = false
+    generateApiDocumentation = false
+    generateApiTests = false
+    generateModelTests = false
+    enablePostProcessFile = true
+
+    templateDir = "$projectDir/src/main/resources/templates"
+    configFile = "$projectDir/src/main/resources/generator-config.yaml"
+    outputDir = "$rootDir/xap-sdk/src/main/kotlin"
+
+    additionalProperties.put(CodegenConstants.ENUM_PROPERTY_NAMING, "UPPERCASE")
+    additionalProperties.put("allowedMediaTypes", AllowedMediaTypesLambda())
+
+    configOptions.put("sourceFolder", "")
+
+    globalProperties.put("supportingFiles", "Room.kt")
+}
+
+tasks.named("openApiGenerate").configure {
+    doLast {
+        exec {
+            commandLine(
+                "../gradlew ktlintFormat".split(" "),
+            )
+            workingDir = File("$rootDir/xap-sdk").absoluteFile
+        }
+    }
+}
