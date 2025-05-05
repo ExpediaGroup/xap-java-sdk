ceb1502902c8ed174bfd2158d18d9aa5b892b641
Omar Aljarrah <50204418+OmarAlJarrah@users.noreply.github.com>
2025-05-05 20:08:13 +0300

    chore: add dokka plugin for documentation generation and versioning (#104)

--- xap-sdk/build.gradle.kts
+++ xap-sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("signing")
     id("maven-publish")
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = property("GROUP_ID") as String
@@ -8,10 +9,35 @@ version = property("VERSION") as String
 
 dependencies {
     api("com.expediagroup:expediagroup-sdk-rest:0.0.6-alpha")
+
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
     runtimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+
     implementation("org.slf4j:slf4j-api:2.0.17")
+
+    dokkaHtmlPlugin("org.jetbrains.dokka:versioning-plugin:2.0.0")
+}
+
+dokka {
+    moduleName = "XAP SDK"
+
+    pluginsConfiguration {
+        html {
+            val dokkaAssets = File(project.findProperty("dokka-assets.location").toString())
+
+            customAssets.from(File(dokkaAssets, "logo-icon.svg"))
+            customStyleSheets.from(File(dokkaAssets, "custom-styles.css"))
+        }
+
+        versioning {
+            version = project.property("VERSION") as String
+
+            olderVersionsDir.set(File(project.findProperty("dokka-old-versions.location").toString()))
+
+            renderVersionsNavigationOnAllPages = true
+        }
+    }
 }
 
 apply("$rootDir/gradle-tasks/publish.gradle.kts")
